332476c07ebf73d14617fddd7a36e95da6e0cb0c
Kim Taek <jinze1991@icloud.com>
2023-06-13 00:12:39 +0900

    Update registry to optional

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,12 @@ if [[ "x$PLUGIN_ACCOUNT" == "x" ]]; then
   PLUGIN_ACCOUNT="deployer"
 fi
 
+IMAGE=${PLUGIN_REGISTRY}/${PLUGIN_REPOSITORY}:${PLUGIN_TAG}
+
+if [[ "x$PLUGIN_REGISTRY" == "x" ]]; then
+  IMAGE=${PLUGIN_REPOSITORY}:${PLUGIN_TAG}
+fi
+
 echo ${PLUGIN_TOKEN} | base64 -d > token
 echo ${PLUGIN_CERT} | base64 -d > ca
 
@@ -17,7 +23,8 @@ kubectl config set-credentials ${PLUGIN_ACCOUNT} --token="$(cat token)"
 kubectl config set-context default --cluster=default --user=${PLUGIN_ACCOUNT}
 kubectl config use-context default
 
-kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${PLUGIN_DEPLOYMENT} ${PLUGIN_CONTAINER}=${PLUGIN_REGISTRY}/${PLUGIN_REPOSITORY}:${PLUGIN_TAG} --record
+kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${PLUGIN_DEPLOYMENT} ${PLUGIN_CONTAINER}=${IMAGE} --record
+echo "kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${PLUGIN_DEPLOYMENT} ${PLUGIN_CONTAINER}=${IMAGE} --record"
 
 if [ ! -z ${PLUGIN_WATCH} ]; then
   kubectl -n ${PLUGIN_NAMESPACE} rollout status deployment/${PLUGIN_DEPLOYMENT} --watch --timeout 3m
